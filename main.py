ae15bd2c9b0bbe394ea6e45502b6fa7966bd840d
nishraptor <nmysore@ucsd.edu>
2020-12-27 13:07:27 -0800

    Added the pruning strategy - There are a set number of pruning iterations. The  model is trained at each iteration, then a percentage of weights are removed, and the model weights are reinitialized to the original weights. This operates based on a mask that determines which weights are on/off

--- main.py
+++ main.py
@@ -5,17 +5,67 @@ import torchvision
 from torchvision.transforms import transforms
 from models import LeNet
 from torch import nn
-from collections import defaultdict
 import matplotlib.pyplot as plt
 from config import cfg
+import numpy as np
+
+def create_mask(model):
+
+    step = sum([1 for name, param in model.named_parameters if 'weight' in name])
+    mask = [None] * step
+    step = 0
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            mask[step] = np.ones_like(param.data.cpu().numpy())
+            step = step + 1
+
+    return mask
+
+def prune(percent, model, mask):
+    step = 0
+
+    for name, param in model.named_parameters():
+
+        if 'weight' in name:
+            tensor = param.data.cpu().numpy()
+            nonzero = tensor[np.nonzero[tensor]]
+            percentile = np.percentile(abs(nonzero), percent) # Get the value where 'percent' values are less than
+
+            new_mask = np.where(abs(tensor) < percentile, 0, mask[step])
+
+            param.data = torch.from_numpy(tensor * new_mask).to('cuda')
+            mask[step] = new_mask
+            step += 1
+
+    return model, mask
+
+def original_init(model, mask, state_dict):
+    step = 0
+
+    for name,param in model.named_parameters():
+        if 'weight' in name:
+            param.data = torch.from_numpy(mask[step] * state_dict[name].cpu(().numpy())).to('cuda')
+            step += 1
+        if 'bias' in name:
+            param.data = state_dict[name]
+    return model
 
 
 def train(model,train_loader,test_loader,optimizer,criterion):
 
-    training_loss = defaultdict(int)
+    mask = create_mask(model)
+    init_state = model.state_dict().copy()
+
+    training_loss = np.zeros(cfg['epochs'])
     #training_accuracy = defaultdict(int)
-    testing_loss = defaultdict(int)
-    testing_accuracy = defaultdict(int)
+    testing_loss = np.zeros(cfg['epochs'])
+    testing_accuracy = np.zeros(cfg['epochs'])
+
+    for prune_iteration in range(cfg['prune_iterations']):
+        if prune_iteration != 0:
+            model, mask = prune(cfg['prune_percent'], model, mask)
+            model = original_init(model, mask, init_state)
 
     for i in range(cfg['epochs']):
         print('Epoch', i)
@@ -45,18 +95,22 @@ def train(model,train_loader,test_loader,optimizer,criterion):
 
         #testing
         correct = 0
+        total = 0
         with torch.no_grad():
             for images,labels in test_loader:
                 images = images.cuda()
                 labels = labels.cuda()
 
                 output = model(images)
+
                 loss = criterion(output,labels)
                 testing_loss[i] += loss.item()
 
                 _, pred = torch.max(output.data,1)
+                total += labels.size(0)
                 testing_accuracy[i] += (pred == labels).sum().item()
 
+        print('Accuracy: ', testing_accuracy[i]/total)
         testing_loss[i] /= len(test_loader)
 
 
@@ -66,7 +120,7 @@ def main():
     transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
 
     train_MNIST = torchvision.datasets.MNIST('data/', train = True, download = True, transform = transform)
-    test_MNIST =  torchvision.datasets.MNIST('data/', train = True, download = True, transform = transform)
+    test_MNIST =  torchvision.datasets.MNIST('data/', train = False, download = True, transform = transform)
 
     train_loader = torch.utils.data.DataLoader(train_MNIST,
                                               batch_size=cfg['batch_size'],
@@ -80,7 +134,6 @@ def main():
     model = LeNet().cuda()
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), cfg['learning_rate'])
-
 
     model,training_loss, testing_loss, testing_accuracy = train(model, train_loader,test_loader, optimizer, criterion)
 
